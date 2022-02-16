18e96f2abb1a3d437cfba0d296943d31835843ca
m.r.m <mtc.fx9@gmail.com>
2022-02-16 20:59:00 +0330

    alitest

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\http\controllers\testcontroller;
+use app\http\controllers\writercontroller;
 
 
 Route::get('/', function () {
 
     return view('blog-list');
 });
+
+Route::any('writerslogin',[writercontroller::class,'writerslogin']);
